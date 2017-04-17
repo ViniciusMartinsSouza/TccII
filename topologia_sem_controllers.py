0a1ff3f59fbcaa3e30d315b3f6b409b39568a51e
Vinicius Martins de Souza <viniciusmartinsdesouza@hotmail.com>
2017-04-16 22:43:20 -0300

    alteração no script

--- topologia_sem_controllers.py
+++ topologia_sem_controllers.py
@@ -4,25 +4,30 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Link
 
+
 def topology():
 
-	net = Containernet(controller=Controller)
 
-	h0 = net.addHost('h1')
-    h1 = net.addHost('h2')
-    h2 = net.addHost('h3')
-    h3 = net.addHost('h4')
+    net = Containernet(controller=Controller)
 
-    s0 = net.addSwitch('s1')
-    s1 = net.addSwitch('s2')
 
+    info('*** Adding hosts\n')
+    h0 = net.addHost('h0')
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+
+
+    info('*** Adding switch\n')
+    s0 = net.addSwitch('s0')
+    s1 = net.addSwitch('s1')
+
+    info('*** Creating links\n')
     net.addLink(h0, s0)
     net.addLink(h1, s1)
     net.addLink(h2, s0)
     net.addLink(h3, s1)
-
-    net.start()
-
+   
 if __name__ == '__main__':
     setLogLevel('info')
     topology()

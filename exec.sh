7a453ec07e52597fdf20ed67aaa3dc3337141c13
Vinicius Martins de Souza <viniciusmartinsdesouza@hotmail.com>
2017-06-07 02:16:52 -0300

    results

--- exec.sh
+++ exec.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -v
 
-sh clean.sh 
-sudo python topologia_iperf_I.py h0
-sh clean.sh 
-sudo python topologia_iperf_I.py h1
-sh clean.sh 
-sudo python topologia_iperf_I.py h2
-sh clean.sh 
-sudo python topologia_iperf_I.py h3
-sh clean.sh 
-sudo python topologia_iperf_II.py h0
-sh clean.sh
-sudo python topologia_iperf_II.py h2
-sh clean.sh
-sudo python topologia_iperf_II.py h3
-sh clean.sh
-sudo python topologia_iperf_II.py h4
-sh clean.sh
+date +"%T" > log
+
+#sh clean.sh 
+#sudo python topologia_iperf_I.py h0
+#sh clean.sh 
+#sudo python topologia_iperf_I.py h1
+#sh clean.sh 
+#sudo python topologia_iperf_I.py h2
+#sh clean.sh 
+#sudo python topologia_iperf_I.py h3
+#sh clean.sh 
+#sudo python topologia_iperf_II.py h0
+#sh clean.sh
+#sudo python topologia_iperf_II.py h1
+#sh clean.sh
+#sudo python topologia_iperf_II.py h2
+#sh clean.sh
+#sudo python topologia_iperf_II.py h3
+#sh clean.sh
 
 #sudo python topologia_rtt_I.py 0 0ms 20
 #sudo python topologia_rtt_I.py 0 5ms 20
@@ -30,3 +32,5 @@ sh clean.sh
 #sudo python topologia_rtt_II.py 0 15ms 20
 #sudo python topologia_rtt_II.py 0 20ms 20
 #sudo python topologia_rtt_II.py 0 25ms 20
+
+date +"%T" >> log
